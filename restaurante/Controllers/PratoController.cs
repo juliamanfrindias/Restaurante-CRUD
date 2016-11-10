3908a15214401b56a8c61d23506bc6e7f933af0c
juliamanfrin <juliamanfrindias@gmail.com>
2016-11-10 18:05:56 -0200

    teste

---
+++ restaurante/Controllers/PratoController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantSystem.Models.ViewModel;
+
+namespace RestaurantSystem.Controllers
+{
+    public class PratoController : Controller
+    {
+        private TesteEntities db = new TesteEntities();
+
+        // GET: Prato
+        public ActionResult Index(int id)
+        {
+            var prato = db.Prato.Include(p => p.Restaurante).Include(p => p.Restaurante1);
+            //int id = prato.First().restId;
+            var prat = db.Prato.SqlQuery("select * from dbo.Prato p where p.restId = @p0", id).ToList();
+            //return View(prato.ToList());
+            return View(prat);
+        }
+
+        // GET: Prato/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prato prato = db.Prato.Find(id);
+            if (prato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prato);
+        }
+
+        // GET: Prato/Create
+        public ActionResult Create()
+        {
+            ViewBag.restId = new SelectList(db.Restaurante, "restId", "restName");
+            ViewBag.restId = new SelectList(db.Restaurante, "restId", "restName");
+            return View();
+        }
+
+        // POST: Prato/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "pratoId,restId,pratoName,pratoPreco")] Prato prato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Prato.Add(prato);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.restId = new SelectList(db.Restaurante, "restId", "restName", prato.restId);
+            ViewBag.restId = new SelectList(db.Restaurante, "restId", "restName", prato.restId);
+            return View(prato);
+        }
+
+        // GET: Prato/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prato prato = db.Prato.Find(id);
+            if (prato == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.restId = new SelectList(db.Restaurante, "restId", "restName", prato.restId);
+            ViewBag.restId = new SelectList(db.Restaurante, "restId", "restName", prato.restId);
+            return View(prato);
+        }
+
+        // POST: Prato/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "pratoId,restId,pratoName,pratoPreco")] Prato prato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(prato).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.restId = new SelectList(db.Restaurante, "restId", "restName", prato.restId);
+            ViewBag.restId = new SelectList(db.Restaurante, "restId", "restName", prato.restId);
+            return View(prato);
+        }
+
+        // GET: Prato/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prato prato = db.Prato.Find(id);
+            if (prato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prato);
+        }
+
+        // POST: Prato/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Prato prato = db.Prato.Find(id);
+            db.Prato.Remove(prato);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ restaurante/Controllers/RestauranteController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantSystem.Models.ViewModel;
+
+namespace RestaurantSystem.Controllers
+{
+    public class RestauranteController : Controller
+    {
+        private TesteEntities db = new TesteEntities();
+
+        // GET: Restaurante
+        public ActionResult Index()
+        {
+            return View(db.Restaurante.ToList());
+        }
+
+        // GET: Restaurante/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurante restaurante = db.Restaurante.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+            
+            //var teste = db.Prato.SqlQuery("select * from dbo.Prato p where p.restId = id");
+            return View(restaurante);
+        }
+
+        // GET: Restaurante/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurante/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "restId,restName")] Restaurante restaurante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurante.Add(restaurante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurante);
+        }
+
+        // GET: Restaurante/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurante restaurante = db.Restaurante.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurante);
+        }
+
+        // POST: Restaurante/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "restId,restName")] Restaurante restaurante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(restaurante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurante);
+        }
+
+        // GET: Restaurante/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurante restaurante = db.Restaurante.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurante);
+        }
+
+        // POST: Restaurante/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Restaurante restaurante = db.Restaurante.Find(id);
+            db.Restaurante.Remove(restaurante);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ShowPrato(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurante restaurante = db.Restaurante.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+           
+            return RedirectToAction("Index", "Prato", new { id = id});
+
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
